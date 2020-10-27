a3301e5a5ac92dd1daa78591020e42899c989a6d
carlos diaz <eduarbboy12@gmail.com>
2020-10-26 21:27:18 -0500

    Ajuste pruebas y creacion del Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,5 @@
+node {
+    stage('Scm Checkout') {
+        git credentialsId: 'd7be3735-e418-48f8-ac8a-8e220d54c042', url: 'https://github.com/kaitus/springboot-jenkins-maven.git'
+    }
+}
--- src/test/java/com/springboot/reactor/apirest/apirest/ApiRestApplicationTests.java
+++ src/test/java/com/springboot/reactor/apirest/apirest/ApiRestApplicationTests.java
@@ -10,8 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
-
 import java.util.Collections;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ApiRestApplicationTests {
